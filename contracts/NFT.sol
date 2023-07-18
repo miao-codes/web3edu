2029e8fc6474a073cc9b4e5a503124075536a72c
miao-codes <yunjiccc@gmail.com>
2023-07-18 14:03:36 +0900

    20230718 14:03

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,37 +1,44 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 {
-    string public uri;
+contract NFT is ERC721, Ownable {
+    string public baseUri;
+    string public fileExtention = ".json";
+    using Strings for uint256;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _tokenId
-    ) ERC721(_name, _symbol) {
-        _mint(msg.sender, _tokenId);
+    constructor(string memory _baseuri) ERC721("MyNFT", "MYN") {
+        setBaseURI(_baseuri);
     }
 
     function setBaseURI(string memory _uri) public {
-        uri = _uri;
+        baseUri = _uri;
     }
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return uri;
+    function baseURI() external view returns (string memory) {
+        return baseUri;
+        //return "ipfs://QmeSrpyEPVGxAfmfkL8R5TSwQgDSgnUSBC165sBFQqkZcF/";
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
     }
 
     function tokenURI(
         uint256 tokenId
     ) public view virtual override returns (string memory) {
-        _requireMinted(tokenId);
-
-        // string memory fileName = string.concat(Strings.toString(tokenId), ".png");
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        // string memory baseURI = baseURI();
         return
-            bytes(uri).length > 0
-                ? string.concat(uri, Strings.toString(tokenId), ".png")
+            bytes(baseUri).length > 0
+                ? string(
+                    abi.encodePacked(baseUri, tokenId.toString(), fileExtention)
+                )
                 : "";
     }
 }
